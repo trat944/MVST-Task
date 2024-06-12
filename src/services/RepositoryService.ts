00dec3ee7d879e043327975ea93953f715ac270a
Jorge Revuelta <jrjrevuelta@gmail.com>
2024-06-12 18:32:56 +0200

    petitions to backend typed with interfaces

--- src/services/RepositoryService.ts
+++ src/services/RepositoryService.ts
@@ -1,15 +1,17 @@
 import axios from "axios"
+import { Repository } from "../interfaces/repository";
 
 const { VITE_BASE_URL } = import.meta.env
 
 export class RepositoryService {
-  static async getRepositoriesFromUser(username: string) {
+  static async getRepositoriesFromUser(username: string): Promise<Repository[]> {
     try {
       const response = await axios.get(`${VITE_BASE_URL}/users/${username}/repos`);
       console.log(response.data)
       return response.data;
     } catch (error) {
       console.log(error);
+      return [];
     }
   }
 }
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,18 +1,21 @@
 import axios from "axios"
+import { User } from "../interfaces/user";
+import { UserSpecific } from "../interfaces/userSpecific";
 
 const { VITE_BASE_URL } = import.meta.env
 
 export class UserService {
-  static async getUsers() {
+  static async getUsers(): Promise<User[]> {
     try {
       const response = await axios.get(`${VITE_BASE_URL}/users`);
       console.log(response.data);
       return response.data;
     } catch (error) {
       console.log(error);
+      return [];
     }
   }
-  static async getSingleUser(username: string) {
+  static async getSingleUser(username: string): Promise<UserSpecific | undefined> {
     try {
       const response = await axios.get(`${VITE_BASE_URL}/users/${username}`);
       console.log(response.data);
