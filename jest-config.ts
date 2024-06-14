ed2eeb87fcbf978c91397ec3c1ed363457c57c2f
Jorge Revuelta <jrjrevuelta@gmail.com>
2024-06-15 00:00:33 +0200

    testing in progress

--- jest-config.ts
+++ jest-config.ts
@@ -7,8 +7,16 @@ const config: Config.InitialOptions = {
     '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testMatch: ['**/__tests__/**/*.test.ts?(x)'],
+  testMatch: ['**/__tests__/**/*.(test|spec).ts?(x)'],
+  moduleNameMapper: {
+    '^@interfaces/(.*)$': '<rootDir>/src/interfaces/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@services/(.*)$': '<rootDir>/src/services/$1'
+  },
 };
 
 export default config;
+
 
